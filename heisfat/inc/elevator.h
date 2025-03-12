919a993f72e54b0081ee242b74c09b466d559254
Joemgu <jegunnarshaug@gmail.com>
2025-03-12 14:58:49 +0100

    la til doxgen kommentarer i elevator.c og elevator.h NEXT: legg til kommentarer til resten

--- heisfat/inc/elevator.h
+++ heisfat/inc/elevator.h
@@ -5,44 +5,137 @@
 #include <stdio.h>
 
 // Enum for the different states of the elevator
+/**
+ * @brief Enum for the different states of the elevator.
+ */
 typedef enum {
-    IDLE,
-    MOVING,
-    AT_DESTINATION,
-    EMERGENCY
+    IDLE,           /**< Elevator is idle, not moving */
+    MOVING,         /**< Elevator is currently moving */
+    AT_DESTINATION, /**< Elevator has reached the destination floor */
+    EMERGENCY       /**< Elevator is in an emergency state */
 } Elevator_state_t; 
 
-//Test, kanskje bruk senere
-//typedef void(*state_handler_ptr_fun)(Elevator* anElevator);
+// Test, maybe use later
+// typedef void(*state_handler_ptr_fun)(Elevator* anElevator);
 
+/**
+ * @brief Structure to represent the elevator's state and properties.
+ */
 typedef struct {
-    Elevator_state_t current_state; // current state of the elevator
-    int current_floor; // current floor of the elevator
-    int destination_floor; // next order to be executed
-    bool has_destination; // bool to determine if the elevator has a destination floor
-    bool moving_up;
-    time_t start_time;
+    Elevator_state_t current_state; /**< The current state of the elevator */
+    int current_floor;             /**< The current floor of the elevator */
+    int destination_floor;         /**< The next order to be executed */
+    bool has_destination;          /**< Boolean to indicate if the elevator has a destination floor */
+    bool moving_up;                /**< Boolean indicating the direction of movement (true = up, false = down) */
+    time_t start_time;             /**< Time the elevator started its movement */
 } Elevator;
 
-//Function that updates the destination floor (will be used by the order handler)
+/**
+ * @brief Set the current state of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param a_current_state The state to set for the elevator.
+ */
 void set_current_state(Elevator *anElevator, Elevator_state_t a_current_state);
+
+/**
+ * @brief Get the current state of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The current state of the elevator.
+ */
 Elevator_state_t get_current_state(Elevator *anElevator);
 
-//updates the destination floor of anElevator to the given int value(must be between 0 and 3)
-void set_destination_floor(Elevator *anElevator,int destination_floor);
+/**
+ * @brief Set the destination floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param destination_floor The destination floor to set (must be between 0 and 3).
+ */
+void set_destination_floor(Elevator *anElevator, int destination_floor);
+
+/**
+ * @brief Get the destination floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The destination floor of the elevator.
+ */
 int get_destination_floor(Elevator *anElevator);
 
+/**
+ * @brief Set the current floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param a_current_floor The current floor to set for the elevator.
+ */
 void set_current_floor(Elevator *anElevator, int a_current_floor);
+
+/**
+ * @brief Get the current floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The current floor of the elevator.
+ */
 int get_current_floor(Elevator *anElevator);
 
+/**
+ * @brief Set whether the elevator has a destination.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param has_a_destination True if the elevator has a destination floor, false otherwise.
+ */
 void set_has_destination(Elevator *anElevator, bool has_a_destination);
+
+/**
+ * @brief Get whether the elevator has a destination.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return True if the elevator has a destination floor, false otherwise.
+ */
 bool get_has_destination(Elevator *anElevator);
 
+/**
+ * @brief Set the start time of the elevator's movement.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ */
 void set_start_time(Elevator *anElevator);
+
+/**
+ * @brief Get the time difference in seconds since the elevator started moving.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The time difference in seconds since the elevator started moving.
+ */
 int get_time_difference(Elevator *anElevator);
 
+/**
+ * @brief Set the direction of movement of the elevator (up or down).
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param moving_up True if the elevator is moving up, false if moving down.
+ */
 void set_moving_up(Elevator *anElevator, bool moving_up);
+
+/**
+ * @brief Get the direction of movement of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return True if the elevator is moving up, false if moving down.
+ */
 bool get_moving_up(Elevator *anElevator);
+
+/**
+ * @brief Switch the direction of movement of the elevator (up to down or down to up).
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ */
 void switch_moving_direction(Elevator *anElevator);
 
+/**
+ * @brief Check if the elevator is currently moving.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return True if the elevator is moving, false if it is idle or at the destination.
+ */
 bool get_is_moving(Elevator *anElevator);
--- heisfat/source/elevator.c
+++ heisfat/source/elevator.c
@@ -1,21 +1,38 @@
 #include "../inc/elevator.h"
 
+/**
+ * @brief Set the current state of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param a_current_state The state to set for the elevator.
+ */
 void set_current_state(Elevator *anElevator, Elevator_state_t a_current_state)
 {
     anElevator->current_state = a_current_state;
 }
 
+/**
+ * @brief Get the current state of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The current state of the elevator.
+ */
 Elevator_state_t get_current_state(Elevator *anElevator)
 {
     return anElevator->current_state;
 }
 
-
-void set_destination_floor(Elevator *anElevator,int destination_floor)
+/**
+ * @brief Set the destination floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param destination_floor The destination floor to set.
+ */
+void set_destination_floor(Elevator *anElevator, int destination_floor)
 {
     if(destination_floor < 0 || destination_floor > N_FLOORS)
     {
-        //later implent interupt
+        //later implement interrupt
         //prints "Floor out of bound"
         printf("Floor out of bound\n");
     }
@@ -23,44 +40,92 @@ void set_destination_floor(Elevator *anElevator,int destination_floor)
     return;
 };
 
-
+/**
+ * @brief Get the destination floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The destination floor of the elevator.
+ */
 int get_destination_floor(Elevator *anElevator)
 {
     return anElevator->destination_floor;
 }
 
+/**
+ * @brief Set the current floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param a_current_floor The current floor to set for the elevator.
+ */
 void set_current_floor(Elevator *anElevator, int a_current_floor)
 {
     anElevator->current_floor = a_current_floor;
 }
 
+/**
+ * @brief Get the current floor of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The current floor of the elevator.
+ */
 int get_current_floor(Elevator *anElevator)
 {
     return anElevator->current_floor;
 }
 
+/**
+ * @brief Set whether the elevator has a destination.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param has_a_destination True if the elevator has a destination, false otherwise.
+ */
 void set_has_destination(Elevator *anElevator, bool has_a_destination)
 {
     anElevator->has_destination = has_a_destination;
 }
 
+/**
+ * @brief Get whether the elevator has a destination.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return True if the elevator has a destination, false otherwise.
+ */
 bool get_has_destination(Elevator *anElevator)
 {
     return anElevator->has_destination;
 }
 
+/**
+ * @brief Set the moving direction of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @param moving_up True if the elevator is moving up, false if moving down.
+ */
 void set_moving_up(Elevator *anElevator, bool moving_up)
 {
     anElevator->moving_up = moving_up;
 }
 
-bool get_moving_up(Elevator *anElevator){
+/**
+ * @brief Get the moving direction of the elevator.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return True if the elevator is moving up, false if moving down.
+ */
+bool get_moving_up(Elevator *anElevator)
+{
     return anElevator->moving_up;
 }
 
+/**
+ * @brief Switch the moving direction of the elevator (up or down).
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ */
 void switch_moving_direction(Elevator *anElevator)
 {
-    if(anElevator->moving_up){
+    if(anElevator->moving_up)
+    {
         anElevator->moving_up = false;
     } 
     else 
@@ -70,16 +135,33 @@ void switch_moving_direction(Elevator *anElevator)
     return;
 }
 
+/**
+ * @brief Set the start time of the elevator's movement.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ */
 void set_start_time(Elevator *anElevator)
 {
     anElevator->start_time = time(NULL);
 }
 
+/**
+ * @brief Get the time difference since the elevator started moving.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return The time difference in seconds since the elevator started moving.
+ */
 int get_time_difference(Elevator *anElevator)
 {
     return (int)time(NULL) - anElevator->start_time;
 }
 
+/**
+ * @brief Check if the elevator is currently moving.
+ * 
+ * @param anElevator Pointer to the Elevator object.
+ * @return True if the elevator is moving, false if it is not.
+ */
 bool get_is_moving(Elevator *anElevator) 
 {
     if ((anElevator->current_state) == MOVING)
